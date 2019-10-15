719c2b0ee1bfba80290304aad82cf262c492f313
Mohd Ali <mohd.ali@nintex.com>
2019-10-15 17:50:37 +0800

    Add project files.

---
+++ BlazorCart/Data/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCart.Data
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
